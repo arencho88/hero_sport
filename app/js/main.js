75e1a46f0d7523cdaadecc494130db24d951d19c
arencho88 <california0107@mail.ru>
2020-12-16 21:24:10 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,14 @@ $(function(){
 	$('.slider__box').slick({
 		prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/slider-arrow.png"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="images/icons/slider-arrow.png"></button>',
+		responsive: [
+			{
+				breakpoint: 700,
+				settings: {
+					arrows: false,
+				}
+			}
+		]
 	});
 
 
@@ -10,6 +18,14 @@ $(function(){
 		dots: true,
 		infinite: false,
 		centerMode: true,
-		centerPadding: '26%'
+		centerPadding: '26%',
+		responsive: [
+			{
+				breakpoint: 700,
+				settings: {
+					// centerPadding: '0',
+				}
+			}
+		]
 	});
 });
